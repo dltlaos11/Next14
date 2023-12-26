ddc263cd71a5a735962b591aa9521a96ccb861ee
dltlaos11 <dltlaos14@naver.com>
2023-12-26 21:32:46 +0900

    feat: msw 세팅

---
+++ j-com/src/mocks/http.ts
@@ -0,0 +1,18 @@
+import { createMiddleware } from "@mswjs/http-middleware";
+import express from "express";
+import cors from "cors";
+import { handlers } from "./handlers";
+
+const app = express();
+const port = 9090; // server
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // cli
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(createMiddleware(...handlers));
+app.listen(port, () => console.log(`Mock server is running on port: ${port}`));
